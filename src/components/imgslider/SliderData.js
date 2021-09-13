13e9706044a1e6e15ef98145277f64674e014d9d
Ollie <Oliverjuul9@gmail.com>
2021-09-13 16:04:34 +0200

    .

--- src/components/imgslider/SliderData.js
+++ src/components/imgslider/SliderData.js
@@ -1,6 +1,12 @@
 import { img1 } from '../../img/sliderimage1.png'
+import { img2 } from '../../img/sliderimage2.png'
 export const SliderData = [
     {
-        image: ''
+        image: { img1 },
+        alt: 'Banner image'
+    },
+    {
+        image: { img2 },
+        alt: 'Banner image'
     }
 ]
